fab523a2e8156bd21b7c844e65650acfba1bfd63
gateniomer <gateniomer@gmail.com>
2022-03-30 16:35:23 +0300

    update js

--- JavaScript/Project 6 - Git Finder/ui.js
+++ JavaScript/Project 6 - Git Finder/ui.js
@@ -4,6 +4,7 @@ class UI{
   }
 
   showProfile(user){
+    console.log(user);
     this.profile.innerHTML = `
     <div class="card card-body mb-3">
         <div class="row">
@@ -12,10 +13,10 @@ class UI{
             <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
           </div>
           <div class="col-md-9">
-            <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
-            <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
-            <span class="badge badge-success">Followers: ${user.followers}</span>
-            <span class="badge badge-info">Following: ${user.following}</span>
+            <span class="badge bg-primary">Public Repos: ${user.public_repos}</span>
+            <span class="badge bg-secondary">Public Gists: ${user.public_gists}</span>
+            <span class="badge bg-success">Followers: ${user.followers}</span>
+            <span class="badge bg-info">Following: ${user.following}</span>
             <br><br>
             <ul class="list-group">
               <li class="list-group-item">Company: ${user.company}</li>
