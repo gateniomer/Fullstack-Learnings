3b94c91f86fa9b5149230d44f038857398e0fdfd
gateniomer <gateniomer@gmail.com>
2022-03-30 15:34:56 +0300

    update js

---
+++ JavaScript/Project 6 - Git Finder/github.js
@@ -0,0 +1,17 @@
+class Github{
+  constructor() {
+    this.client_id = 'addc79b00426a5355775';
+    
+    this.client_secret = 'e3293b659fe10a5050c61695f9eb53d8eb120a8d';
+  }
+
+  async getUser(user){
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+
+    return{
+      profile
+    }
+  }
+}
