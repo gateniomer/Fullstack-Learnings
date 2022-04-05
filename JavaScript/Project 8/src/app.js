27bc4a8cc06ef1707de758b4d8979339c06b3adc
omergat <gateniomer@gmail.com>
2022-04-05 19:16:56 +0300

    updated js file

--- JavaScript/Project 8/src/app.js
+++ JavaScript/Project 8/src/app.js
@@ -1,2 +1,9 @@
-const greeting = 'Hello World';
-console.log(greeting);
+import {http} from './http';
+
+document.addEventListener('DOMContentLoaded',getPosts);
+
+function getPosts(){
+  http.get('http://localhost:3000/posts')
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
+}
