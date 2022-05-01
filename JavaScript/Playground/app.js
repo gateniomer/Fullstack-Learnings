bc3e0f40996c77034a4099b7221bd8b9647de2c0
omergat <gateniomer@gmail.com>
2022-05-01 18:31:41 +0300

    updated files

--- JavaScript/Playground/app.js
+++ JavaScript/Playground/app.js
@@ -1,15 +1,15 @@
 // const multiplyBy = function(multiplier){
 //   return function(num){return num*multiplier};
 // }
-const multiplyBy = (multiplier) => (num) => multiplier * num;
+// const multiplyBy = (multiplier) => (num) => multiplier * num;
 
-console.log(multiplyBy(6)(4));
+// console.log(multiplyBy(6)(4));
 
-const multiplyByTwo = multiplyBy(2);
-const multiplyByTen = multiplyBy(10);
+// const multiplyByTwo = multiplyBy(2);
+// const multiplyByTen = multiplyBy(10);
 
-console.log(multiplyByTwo(10));
-console.log(multiplyByTen(10));
+// console.log(multiplyByTwo(10));
+// console.log(multiplyByTen(10));
 
 const attackType = (type = 'melee') => (range = 1) => console.log(`${type} attack with range of ${range}`);
 
@@ -17,3 +17,4 @@ const spearAttack = attackType();
 const magicBallAttack = attackType('magic');
 console.log(spearAttack());
 console.log(magicBallAttack(4));
+
