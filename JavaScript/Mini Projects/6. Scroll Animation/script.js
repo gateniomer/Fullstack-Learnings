d447d89b216cbd29e880bec5b6829561d7379b2c
omergat <gateniomer@gmail.com>
2022-05-15 18:08:08 +0300

    updated files

--- JavaScript/Mini Projects/6. Scroll Animation/script.js
+++ JavaScript/Mini Projects/6. Scroll Animation/script.js
@@ -1,26 +1,31 @@
 const body = document.querySelector('body');
-let scrollMax=0;
-scrollMax = addCard(scrollMax,body);
-scrollMax = addCard(scrollMax,body);
-scrollMax = addCard(scrollMax,body);
-scrollMax = addCard(scrollMax,body);
-scrollMax = addCard(scrollMax,body);
+for(let i=0;i<10;i++){
+  addCard(i+1,body);
+}
+const cards = document.querySelectorAll('.card');
+let breakpoint = window.innerHeight-200;
+checkForCards();
 addEventListener('scroll',(e)=>{
-  console.log(scrollY);
-  if(screenY>(scrollMax-1000)){
-    console.log('success')
-    scrollMax = addCard(scrollMax,body);
-    
-  }
+  checkForCards();
 });
 
-function addCard(scrollMax,body){
+function checkForCards(){
+  cards.forEach(card => {
+    const cardY = card.getBoundingClientRect().top;
+    console.log(cardY)
+    if(cardY<breakpoint){
+      card.classList.add('show');
+    }else{
+      card.classList.remove('show');
+    }
+  })
+}
+function addCard(counter,body){
   const div = document.createElement('div');
   const h2 = document.createElement('h2');
-  h2.textContent = scrollMax;
+  h2.textContent = counter;
   div.className='card';
   div.appendChild(h2);
   body.appendChild(div);
-  scrollMax+=200;
-  return scrollMax;
+  counter++;
 }
