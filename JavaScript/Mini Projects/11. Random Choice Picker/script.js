01d45f25fae0384cd19fa3eb6354a6af8322ab15
omergat <gateniomer@gmail.com>
2022-05-23 12:37:29 +0300

    updated files

--- JavaScript/Mini Projects/11. Random Choice Picker/script.js
+++ JavaScript/Mini Projects/11. Random Choice Picker/script.js
@@ -3,25 +3,44 @@ const choiceContainer = document.querySelector('#choice-container');
 
 textArea.focus();
 
-textArea.addEventListener('keydown',e => {
+textArea.addEventListener('keyup',e => {
+  //get value from textarea
   const value = e.target.value;
-  const choices = value.split(',');
+  //convet value to array without whitespaces
+  const choices = value.split(',')
+  .filter(choice => choice.trim()!=='')
+  .map(choice=>choice.trim());
+  //add choices from array by adding spans
   choiceContainer.innerHTML = '';
   choices.forEach(choice => {
-    if(choice!=='')
-      choiceContainer.innerHTML+=`
-      <span class= 'choice'>${choice}</span>
-      `
+    choiceContainer.innerHTML+=`
+    <span class= 'choice'>${choice}</span>
+    `
   });
+
   if(e.code === 'Enter'){
-    e.preventDefault();
-    const inter = setInterval(choose,100);
+    //get all choices spans
+    const choices = document.querySelectorAll('.choice');
+    if(choices.length > 0){
+      //set interval to choose random choice
+      const interval = setInterval(()=>chooseRandom(choices),100);
+      //after 3 secondes clear interval and enable textarea again.
+      setTimeout(()=>{
+        clearInterval(interval);
+        e.target.value = '';
+        e.target.disabled=false;
+        e.target.focus();
+      },2000);
+    }
   }
 });
 
-function choose(){
-  const choices = document.querySelectorAll('.choice');
+function chooseRandom(choices){
+  //Disable text area
+  textArea.disabled= true;
+  //remove chosen class from all choices
   choices.forEach(choice => choice.classList.remove('chosen'));
+  //add chosen class to random choice
   const random = Math.floor(Math.random() * choices.length);
   choiceContainer.children[random].classList.add('chosen');
 }
