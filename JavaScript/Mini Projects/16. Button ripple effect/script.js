9aa5906be13455c776cb56b4faa96a6d915fe6b2
omergat <gateniomer@gmail.com>
2022-05-25 17:19:54 +0300

    updated files

---
+++ JavaScript/Mini Projects/16. Button ripple effect/script.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector('.ripple');
+
+btn.addEventListener('click',function(e){
+  const ripple = document.createElement('div');
+  ripple.className = 'circle';
+  ripple.style.top = `${e.clientY - e.target.offsetTop}px`;
+  ripple.style.left = `${e.clientX - e.target.offsetLeft}px`;
+  this.appendChild(ripple);
+
+  setTimeout(()=>ripple.remove(),1000);
+});
