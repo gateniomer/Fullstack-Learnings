14c8e36c124970282e78114da4b4346bcc17a37a
omergat <gateniomer@gmail.com>
2022-05-25 16:54:47 +0300

    ...

--- JavaScript/Mini Projects/15. Theme clock/script.js
+++ JavaScript/Mini Projects/15. Theme clock/script.js
@@ -1,9 +1,19 @@
 const needles = document.querySelectorAll('.needle');
 const needlesArray = Array.from(needles);
+const toggleBtn = document.querySelector('.toggle');
+const html = document.querySelector('html');
+const time = document.querySelector('.time');
+const dateText = document.querySelector('.date');
+toggleBtn.addEventListener('click',e=>{
+  html.classList.toggle('dark');
+  html.classList.contains('dark')?toggleBtn.textContent = 'Light Mode':toggleBtn.textContent = 'Dark Mode';
+});
 setInterval(()=>{
     const date = new Date();
-    console.log(date.getSeconds());
     needles[0].style.transform=`translate(-50%,0) rotateZ(${date.getHours()*30}deg)`
     needles[1].style.transform=`translate(-50%,0) rotateZ(${date.getMinutes()*6}deg)`
     needles[2].style.transform=`translate(-50%,0) rotateZ(${date.getSeconds()*6}deg)`
+
+    time.textContent = `${date.getHours()}:${date.getMinutes()}`;
+    dateText.innerHTML = `<span class="circle">${date.getDate()}</span><span> ${date.getMonth()+1}/${date.getFullYear()}</span>`;
 },1000);
