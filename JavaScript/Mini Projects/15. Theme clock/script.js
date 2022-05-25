c720ce975e5fbe7222c669ac273f074b2faba7cf
omergat <gateniomer@gmail.com>
2022-05-25 16:38:21 +0300

    updated files

--- JavaScript/Mini Projects/15. Theme clock/script.js
+++ JavaScript/Mini Projects/15. Theme clock/script.js
@@ -3,7 +3,7 @@ const needlesArray = Array.from(needles);
 setInterval(()=>{
     const date = new Date();
     console.log(date.getSeconds());
-    needles[0].style.transform=`rotateZ(${date.getHours()*30}deg)`
-    needles[1].style.transform=`rotateZ(${date.getMinutes()*6}deg)`
-    needles[2].style.transform=`rotateZ(${date.getSeconds()*6}deg)`
+    needles[0].style.transform=`translate(-50%,0) rotateZ(${date.getHours()*30}deg)`
+    needles[1].style.transform=`translate(-50%,0) rotateZ(${date.getMinutes()*6}deg)`
+    needles[2].style.transform=`translate(-50%,0) rotateZ(${date.getSeconds()*6}deg)`
 },1000);
