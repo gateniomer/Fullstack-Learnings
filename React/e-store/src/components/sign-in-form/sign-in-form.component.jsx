ac157f734cd518c558a28d796bde189c356eca08
Omer Gatenio <gateniomer@gmail.com>
2022-06-07 18:01:11 +0300

    auth finished

--- React/e-store/src/components/sign-in-form/sign-in-form.component.jsx
+++ React/e-store/src/components/sign-in-form/sign-in-form.component.jsx
@@ -5,12 +5,10 @@ import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
   signInAuthUserWithEmailAndPassword } from '../../utils/firebase/firebase.utils'
-import { useState,useContext } from "react";
+import { useState } from "react";
 import FormInput from "../form-input/form-input.component";
 import './sign-in-form.styles.scss';
 import Button from "../button/button.component";
-
-import { UserContext } from '../../contexts/user.context';
 
 const defaultFormFields = {
   email: '',
@@ -19,8 +17,6 @@ const defaultFormFields = {
 const SignInForm = () => {
   const [formFields,setFormFields] = useState(defaultFormFields);
   const {email,password} = formFields;
-
-  const {setCurrentUser} = useContext(UserContext);
 
   const handleChange = (event) => {
     const {name, value} = event.target;
@@ -36,7 +32,6 @@ const SignInForm = () => {
 
     try{
       const {user} = await signInAuthUserWithEmailAndPassword(email,password);
-      setCurrentUser(user);
       resetFormFields();
     }catch(e){
       switch(e.code){
@@ -54,8 +49,7 @@ const SignInForm = () => {
   }
 
   const signInWithGoogle = async () =>{
-    const {user} = await signInWithGooglePopup();
-    await createUserDocumentFromAuth(user)
+    await signInWithGooglePopup();
   }
 
   return(
