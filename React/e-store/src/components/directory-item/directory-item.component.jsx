c2eac3b6f678458a37f58c8474b3a52bc2f5d097
Omer Gatenio <gateniomer@gmail.com>
2022-06-28 13:57:05 +0300

    styled-components applied

--- React/e-store/src/components/directory-item/directory-item.component.jsx
+++ React/e-store/src/components/directory-item/directory-item.component.jsx
@@ -1,8 +1,12 @@
 import {BackgroundImage,Body,DirectoryItemContainer} from'./directory-item.styles.jsx'
+import { useNavigate } from 'react-router-dom';
 
-const DirectoryItem = ({category:{id,title,imageUrl}}) => {
+const DirectoryItem = ({category:{title,imageUrl,route}}) => {
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route);
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={onNavigateHandler}>
       <BackgroundImage imageUrl={imageUrl}/>
       <Body>
         <h2>{title}</h2>
